85a7df409f4f6a88963231af39da072b6ddd262a
Aikol Tashbolot uulu <imaikol@yahoo.com>
2020-11-14 13:04:04 -0800

    playground-react

---
+++ playground-react/README.md
@@ -0,0 +1,5 @@
+# Playground for React!
+
+
+## All the subjects/learnings are located in the subjects folder as single files,
+## You can copy those files and paste in the src folder and play with it to understand how it works!
---
+++ playground-react/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+        <h1>Playground React!</h1>
+        <img src={logo} style={{height: "10%", width: "10%"}}alt="replacement" />
+    </>
+
+  );
+}
+
+export default App;
